e2e5d7e98a7966ba919caf97bcfe52d292be52ca
Aleksandr Chernopyatov <you@example.com>
2023-09-29 23:13:47 +0400

    add controller LoginController

---
+++ src/main/java/com/flint/Weather/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.flint.Weather.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPAge(){
+        return "login";
+    }
+}
